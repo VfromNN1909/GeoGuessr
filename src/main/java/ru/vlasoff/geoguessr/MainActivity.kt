4679ace41bca6098ed273f8f0d287f5410c15666
vladimir <holdencolfild19@gmail.com>
2021-04-19 19:44:30 +0300

    init project

---
+++ src/main/java/ru/vlasoff/geoguessr/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.vlasoff.geoguessr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
